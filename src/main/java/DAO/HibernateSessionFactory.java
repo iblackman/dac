124265b5f7c766dc090ab57fa485f0199b49b2b5
iblackman <igor.blackman@gmail.com>
2017-06-21 01:55:03 -0300

    alteracao do fillDb() para inserir UserTypes, Users, Rooms...

--- src/main/java/DAO/HibernateSessionFactory.java
+++ src/main/java/DAO/HibernateSessionFactory.java
@@ -1,5 +1,16 @@
 package DAO;
 
+import Model.Events;
+import Model.Eventtypes;
+import Model.Resources;
+import Model.Rooms;
+import Model.Users;
+import Model.Usertypes;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,7 +21,7 @@ public class HibernateSessionFactory {
 
     private static SessionFactory sessionFactory;
     
-    public static Session getSession() {
+    public static Session getSession() throws ParseException {
         if(sessionFactory == null){
             Configuration configuration = new Configuration();
             configuration.configure();
@@ -21,85 +32,79 @@ public class HibernateSessionFactory {
         return sessionFactory.openSession();
     }
     
-    private static void FillDB(){
+    private static void FillDB() throws ParseException{
         Session s = sessionFactory.openSession();
         s.beginTransaction();
         
-//        User user = new User();
-//        user.setName("Igor Antunes");
-//        user.setCity("Rio de Janeiro");
-//        user.setCountry("Brazil");
-//        user.setHost(true);
-//        user.setUsername("igorantunes");
-//        user.setPassword("teste");
-//        user.setProfileURL("fileserver/profiles/igor.jpg");
-//        s.save(user);
-//        
-//        User user2 = new User();
-//        user2.setName("Igor Blackman");
-//        user2.setCity("Niterói");
-//        user2.setCountry("Brazil");
-//        user2.setHost(true);
-//        user2.setUsername("igorblackman");
-//        user2.setPassword("teste");
-//        user2.setProfileURL("fileserver/profiles/igorblackman.jpg");
-//        s.save(user2);
-//        
-//        User user3 = new User();
-//        user3.setName("Paulo Henrique Borges");
-//        user3.setCity("Nova Friburgo");
-//        user3.setCountry("Brazil");
-//        user3.setHost(false);
-//        user3.setUsername("phborges");
-//        user3.setPassword("teste");
-//        user3.setProfileURL("fileserver/profiles/paulo.jpg");
-//        s.save(user3);
-//        
-//        
-//        Rate rate = new Rate();
-//        rate.setSender(user);
-//        rate.setReceiver(user2);
-//        rate.setDescription("Ótima pessoa");
-//        rate.setType(0);
-//        rate.setValue(4);
-//        rate.setCreated(new Date());
-//        s.save(rate);
-//        
-//        rate = new Rate();
-//        rate.setSender(user3);
-//        rate.setReceiver(user2);
-//        rate.setDescription("Ótimo Host");
-//        rate.setType(1);
-//        rate.setValue(5);
-//        rate.setCreated(new Date());
-//        s.save(rate);
-//        
-//        rate = new Rate();
-//        rate.setSender(user);
-//        rate.setReceiver(user2);
-//        rate.setDescription("Péssimo Guest");
-//        rate.setType(2);
-//        rate.setValue(1);
-//        rate.setCreated(new Date());
-//        s.save(rate);
-//        
-//        rate = new Rate();
-//        rate.setSender(user);
-//        rate.setReceiver(user2);
-//        rate.setDescription("Médio SportGuest");
-//        rate.setType(3);
-//        rate.setValue(3);
-//        rate.setCreated(new Date());
-//        s.save(rate);
-//        
-//        rate = new Rate();
-//        rate.setSender(user);
-//        rate.setReceiver(user2);
-//        rate.setDescription("Ótimo SportHost");
-//        rate.setType(4);
-//        rate.setValue(5);
-//        rate.setCreated(new Date());
-//        s.save(rate);
+        //tipos de usuario
+        Usertypes utAdm = new Usertypes();
+        utAdm.setName("Administrator");
+        utAdm.setPermission(1);
+        s.save(utAdm);
+        
+        Usertypes utHost = new Usertypes();
+        utHost.setName("Host");
+        utHost.setPermission(2);
+        s.save(utHost);
+        
+        Usertypes utGuest = new Usertypes();
+        utGuest.setName("Guest");
+        utGuest.setPermission(3);
+        s.save(utGuest);
+        
+        //usuario
+        Users uAdm = new Users();
+        uAdm.setEmail("adm@email.com");
+        uAdm.setLogin("adm");
+        uAdm.setName("ADM");
+        uAdm.setPassword("adm");
+        uAdm.setUsertypeid(utAdm);
+        s.save(uAdm);
+        
+        //resource
+        Resources resAr = new Resources();
+        resAr.setName("Ar condicionado");
+        s.save(resAr);
+        
+        Resources resQuadro = new Resources();
+        resQuadro.setName("Quadro");
+        s.save(resQuadro);
+        
+        //room
+        Rooms room201 = new Rooms();
+        room201.setName("Sala 201");
+        List<Resources> lisResor = new ArrayList<>();
+        lisResor.add(resAr);
+        lisResor.add(resQuadro);
+        room201.setResourcesCollection(lisResor);
+        s.save(room201);
+        
+         //Tipo de evento
+        Eventtypes evtReuniao = new Eventtypes();
+        evtReuniao.setName("Reunião");
+        s.save(evtReuniao);
+        
+        //para gerar a data
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        String dateInString1 = "31-08-2017 10:00:00";
+        String dateInString2 = "31-08-2017 16:00:00";
+        //Date date = sdf.parse(dateInString1);
+        
+        //exemplo de evento
+        Events ev = new Events(1, "name", "description", sdf.parse(dateInString1), sdf.parse(dateInString2), 10);
+        ev.setName("Evento");
+        ev.setDescription("Descricao");
+        ev.setMaxguests(10);
+        ev.setStartdt(sdf.parse(dateInString1));
+        ev.setEnddt(sdf.parse(dateInString2));
+        ev.setEventtypeid(evtReuniao);
+        ev.setRoomid(room201);
+        List<Resources> lisResorEv = new ArrayList<>();
+        lisResorEv.add(resQuadro);
+        ev.setResourcesCollection(lisResorEv);
+        ev.setUserid(uAdm);
+        ev.setStatus(1);// (1-criado, 2-Aguardando aprovacao, 3-Cancelado) 
+        s.save(ev);
         
         
         s.getTransaction().commit();
