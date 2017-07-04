3dcba36de4ae9414a036c6cae673bd927f5a7fa6
iblackman <igor.blackman@gmail.com>
2017-07-03 21:39:23 -0300

    feat: UsersDAO findByEvent

--- src/main/java/DAO/UsersDAO.java
+++ src/main/java/DAO/UsersDAO.java
@@ -5,12 +5,11 @@
  */
 package DAO;
 
+import Model.Events;
 import Model.Users;
-import Model.Usertypes;
+import Model.Eventguests;
+import java.util.ArrayList;
 import java.util.List;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -29,5 +28,22 @@ public class UsersDAO extends GenericDAO<Users, Integer>{
     public List<Users> findAll(){
         List<Users> users = (List<Users>) getSession().createQuery("FROM USERS").list();
         return users;
+    }
+    
+    /**
+     * retorna lista de usuarios que estao inscritos em um evento (Eventguests)
+     * @param event
+     * @return 
+     */
+    public List<Users> findByEvent(Events event){
+        List<Users> result = new ArrayList<>();
+        for (Eventguests evGuest : new EventguestsDAO().findByEvent(event)){
+            result.add(
+                    findById(
+                            evGuest.getUsers().getId()
+                    )
+            );
+        }
+        return result;
     }
 }
