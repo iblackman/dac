4e7c438b2e01890fcc1e9fe3638da5332ac73df7
iblackman <igor.blackman@gmail.com>
2017-07-03 21:37:37 -0300

    feat: Eventguests findByEvent

--- src/main/java/DAO/EventguestsDAO.java
+++ src/main/java/DAO/EventguestsDAO.java
@@ -6,6 +6,7 @@
 package DAO;
 
 import Model.Eventguests;
+import Model.Events;
 import Model.Users;
 import java.util.List;
 /**
@@ -24,10 +25,24 @@ public class EventguestsDAO extends GenericDAO<Eventguests, Integer>{
         List<Eventguests> eventguests = (List<Eventguests>) getSession().createQuery("FROM EVENTGUESTS").list();
         return eventguests;
     }
-    
+    /**
+     * retorna lista de Eventguests de um usuario
+     * @param user
+     * @return 
+     */
     public List<Eventguests> findByUser(Users user){
         return (List<Eventguests>) getSession().createQuery("FROM EVENTGUESTS WHERE users = :user")
                 .setParameter("user", user)
+                .list();
+    }
+    /**
+     * retorna lista de Eventguests que tem o evento passado como parametro
+     * @param event
+     * @return 
+     */
+    public List<Eventguests> findByEvent(Events event){
+        return (List<Eventguests>) getSession().createQuery("FROM EVENTGUESTS WHERE events = :event")
+                .setParameter("event", event)
                 .list();
     }
 }
